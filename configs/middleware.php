9dd05eddc50b1695a54c6e07c6be2021afaada76
Горбачев Денис <gorbyd1987@gmail.com>
2023-11-23 22:20:59 +0300

    my P4_end

--- configs/middleware.php
+++ configs/middleware.php
@@ -3,6 +3,9 @@
 declare(strict_types = 1);
 
 use App\Config;
+use App\Middleware\OldFormDataMiddleware;
+use App\Middleware\StartSessionsMiddleware;
+use App\Middleware\ValidationErrorsMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
 use Slim\Views\Twig;
@@ -12,10 +15,12 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
-    // Twig
-    $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
 
+    $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
     $app->add(ValidationExceptionMiddleware::class);
+    $app->add(ValidationErrorsMiddleware::class);
+    $app->add(OldFormDataMiddleware::class);
+    $app->add(StartSessionsMiddleware::class);
 
     // Logger
     $app->addErrorMiddleware(
