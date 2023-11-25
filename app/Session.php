49f63cf26a801393d398d315bfed65e5045aa73f
Горбачев Денис <gorbyd1987@gmail.com>
2023-11-26 00:41:34 +0300

    my P7_end

---
+++ app/Session.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App;
+
+use App\Contracts\SessionInterface;
+use App\DataObjects\SessionConfig;
+use App\Exception\SessionException;
+
+class Session implements SessionInterface
+{
+
+    public function __construct(private readonly SessionConfig $options)
+    {
+    }
+
+    public function start(): void
+    {
+        if ($this->isActive()) {
+            throw new SessionException('Session has already been started');
+        }
+
+        if (headers_sent($fileName, $line)) {
+            throw new SessionException('Headers already sent by ' . $fileName . ':' . $line);
+        }
+
+        //Если поставить secure => true, то в моей ситуации в данный момент все ломается, т.к. не может установиться кука
+        //с передаваемым session_id. Изза этого сессия пустая и не удается залогиниться. А началось с того, что я экспериментировал с удалением кук в инспекторе браузера
+        //Но у него подключается также без https и не ломается, хз почему
+        session_set_cookie_params(
+            [
+                'secure' => $this->options->secure,
+                'httponly' => $this->options->httponly,
+                'samesite' => $this->options->sameSite->value,
+            ]
+        );
+
+        if (!empty($this->options->name)) {
+            session_name($this->options->name);
+        }
+
+        if (!session_start()) {
+            throw new SessionException('Unable to start the session');
+        }
+    }
+
+    public function save(): void
+    {
+        session_write_close();
+    }
+
+    public function isActive(): bool
+    {
+        return session_status() === PHP_SESSION_ACTIVE;
+    }
+
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->has($key) ? $_SESSION[$key] : $default;
+    }
+
+    public function regenerate(): bool
+    {
+        return session_regenerate_id();
+    }
+
+    public function put(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function forget(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+
+    private function has(string $key) : bool
+    {
+        return array_key_exists($key, $_SESSION);
+    }
+}
