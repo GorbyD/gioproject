37ab21ea63c14203681c74c912582d5065f93df8
Горбачев Денис <gorbyd1987@gmail.com>
2023-11-30 00:00:26 +0300

    my P20_end

--- configs/app.php
+++ configs/app.php
@@ -3,6 +3,7 @@
 declare(strict_types = 1);
 
 use App\Enum\AppEnvironment;
+use App\Enum\StorageDriver;
 
 $appEnv = $_ENV['APP_ENV'] ?? AppEnvironment::Production->value;
 $appSnakeName = strtolower(str_replace(' ', '_', $_ENV['APP_NAME']));
@@ -33,6 +34,9 @@ return [
         'secure' => false, //см. коммент в \App\Session::start()
         'httponly' => true,
         'samesite' => 'lax',
+    ],
+    'storage' => [
+        'driver' => StorageDriver::Local
     ],
 
 ];
