c7412ac3a27e15fb3138d9f7f8e5c82fa6aa07e0
Gorby <gorbyd1987@gmail.com>
2023-12-25 14:32:20 +0300

    my P36_end

--- migrations/Version20231219132513.php
+++ migrations/Version20231219132513.php
@@ -10,8 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231219132513 extends AbstractMigration
-{
+final class Version20231219132513 extends AbstractMigration{
     public function getDescription(): string
     {
         return '';
